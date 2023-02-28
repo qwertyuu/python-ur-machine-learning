7ea3726022a5bd68377b3a5198c98a7e5e58da08
Raphaël Côté <cotlarrc@msn.com>
2023-02-28 07:21:46 -0500

    Update infer.py

--- infer.py
+++ infer.py
@@ -1,19 +1,38 @@
 from flask import Flask, jsonify, request
 import pickle
-import lightgbm as lgb
 from pandas import json_normalize
 from src.prep import convert_game_to_cols
+import os
+import requests
+import shutil
+import dotenv
 
-model_name = 'lgb_classifier.txt'
+dotenv.load_dotenv()
+
+model_name = os.getenv("MODEL_NAME", "model.pkl")
 app = Flask(__name__)
 
+# check that model name is local file or url. If url, download it to local file and use that
+if model_name.startswith("http"):
+    r = requests.get(model_name, stream=True)
+    if r.status_code == 200:
+        print("Downloading model from url")
+        with open("model.pkl", 'wb') as f:
+            r.raw.decode_content = True
+            shutil.copyfileobj(r.raw, f)
+        model_name = "model.pkl"
+    else:
+        raise Exception("Could not download model from url")
+    
 def load():
-    return lgb.Booster(model_file=model_name)
+    print("loading model")
+    with open(model_name, 'rb') as f:
+        model = pickle.load(f)
+    return model
 
 model_pool = [
     load(),
 ]
-
 
 @app.route('/infer', methods=['POST'])
 def infer():
@@ -24,7 +43,6 @@ def infer():
     
     df = df.drop(columns=["game"])
     if len(model_pool) == 0:
-        print("creating model")
         model = load()
     else:
         model = model_pool.pop()
